181758b2ded080f3d31a75a4b8628fd8d8487b77
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-18 23:27:41 +0000

    Init.

---
+++ src/clojure_30_seconds_of_code/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-30-seconds-of-code.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojure_30_seconds_of_code/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-30-seconds-of-code.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-30-seconds-of-code.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
